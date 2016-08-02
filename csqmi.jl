9e28c2e940d69cb279995b40b1c91b4d693fc5d3
Micah Corah <micahcorah@gmail.com>
2016-08-02 17:48:26 -0400

    csqmi updates

--- csqmi.jl
+++ csqmi.jl
@@ -4,25 +4,27 @@ function compute_mutual_information(boundary_ps, applied_p, prior, sigma, interi
 end
 
 function compute_mutual_information(critical_fs, prior, sigma, cell_volume)
+  nu = sigma^2
+
   belief = flatten(map(prior->prior.cells, prior))[:]
 
   field = flatten(critical_fs)[:]
 
-  i1 = integral1(field, belief, cell_volume, sigma)
-  i2 = integral2(belief, cell_volume, sigma)
-  i3 = integral3(field, belief, cell_volume, sigma)
+  i1 = integral1(field, belief, cell_volume, nu)
+  i2 = integral2(belief, cell_volume, nu)
+  i3 = integral3(field, belief, cell_volume, nu)
 
   out = -2*log(e, i1) + log(e, i2) + log(e, i3)
 
   out
 end
 
-function integral1(field, belief, cell_volume, sigma)
+function integral1(field, belief, cell_volume, nu)
   out = 0
 
-  for ii = 1:length(belief)
+  @inbounds @simd for ii = 1:length(belief)
     for jj = 1:length(belief)
-      out += belief[ii]^2 * belief[jj] * normal(field[ii] - field[jj], 2*sigma^2)
+      out += belief[ii]^2 * belief[jj] * normal(field[ii] - field[jj], 2*nu)
     end
   end
   out *= cell_volume^2
@@ -30,24 +32,24 @@ function integral1(field, belief, cell_volume, sigma)
   out
 end
 
-function integral2(belief, cell_volume, sigma)
+function integral2(belief, cell_volume, nu)
   out = 0
 
-  for ii = 1:length(belief)
-    out += belief[ii]^2 * normal(0, 2*sigma^2)
+  @inbounds @simd for ii = 1:length(belief)
+    out += belief[ii]^2 * normal(0, 2*nu)
   end
   out *= cell_volume
 
   out
 end
 
-function integral3(field, belief, cell_volume, sigma)
-  val1 = sum(belief.^2) / cell_volume
+function integral3(field, belief, cell_volume, nu)
+  val1 = sum(belief.^2) * cell_volume
 
   val2 = 0
-  for ii = 1:length(belief)
+  @inbounds @simd for ii = 1:length(belief)
     for jj = 1:length(belief)
-      val2 += belief[ii] * belief[jj] * normal(field[ii] - field[jj], 2*sigma^2)
+      val2 += belief[ii] * belief[jj] * normal(field[ii] - field[jj], 2*nu)
     end
   end
   val2 *= cell_volume^2
