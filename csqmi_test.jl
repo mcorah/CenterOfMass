ddbc3fb0f9096d8fb41bfbc6af46fbecd562be44
Micah Corah <micahcorah@gmail.com>
2016-08-05 19:59:48 -0400

    csqmi_test

---
+++ csqmi_test.jl
@@ -0,0 +1,25 @@
+using PyPlot
+using Convex
+using ProfileView
+using Mapping
+include("CenterOfMass.jl")
+
+function test_csqmi()
+  sigma = 1
+  forces = rand(2000)
+  prior = rand(2000)
+  prior = prior / sum(prior)
+  @elapsed @profile compute_mutual_information(forces, prior, sigma, 1.0)
+end
+
+Profile.clear()
+
+times = Float64[]
+for ii = 1:100
+  time = test_csqmi()
+  push!(times, time)
+end
+
+println("Mean time: $(mean(times))")
+
+ProfileView.view()
