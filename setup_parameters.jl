fac625df3857eb5bab7ce760b360dbf4d9188229
Micah Corah <micahcorah@gmail.com>
2016-08-12 18:00:34 -0400

    updating parameters code

--- setup_parameters.jl
+++ setup_parameters.jl
@@ -2,7 +2,7 @@ min_mass = 0.6
 max_mass = 1.4
 g = -9.8
 #z = [0;0;1]
-sigma = 1
+sigma = 1.0
 
 mass_resolution = 0.2
 masses = collect(min_mass:mass_resolution:max_mass)
@@ -10,16 +10,19 @@ masses = collect(min_mass:mass_resolution:max_mass)
 np = 20
 thetas = 2*pi * (1/np:1/np:1)
 circle_ps = map(theta->[cos(theta);sin(theta);0], thetas)
+circle_ws = to_wrench_matrix(circle_ps)
 
 # move to separate attachment points
 na = 10
 thetas = 2*pi * (1/na:1/na:1)
 r_attach = 0.8
 attachment_ps = map(theta->r_attach*[cos(theta);sin(theta);0], thetas)
+attachment_ws = map(point_to_wrench, attachment_ps)
 
 # set up the occupancy prior
 resolution = 0.1
 
-interior_q(x) = norm(x) <= 1 - resolution * 3/2
+# note resolution
+interior_q(x) = norm(x) <= 1 - 0.1 * 3/2
 
 normalized_error(err) = sqrt(sum(err.*err.*[0.5;0.5;1/(max_mass - min_mass)]))
