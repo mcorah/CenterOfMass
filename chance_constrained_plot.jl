8200a18cd8fc1474a7bfa8bac18cd5e4b0ec775a
Micah Corah <micahcorah@gmail.com>
2016-09-15 12:03:03 -0400

    previous plotting stuff

--- chance_constrained_plot.jl
+++ chance_constrained_plot.jl
@@ -2,16 +2,21 @@ using HDF5, JLD
 using PyCall
 @pyimport matplotlib2tikz
 
+max_immediate_feasibility = 0.476
+mean_immediate_feasibility = 0.322
+
 include("CenterOfMass.jl")
 include("setup_parameters.jl")
 
-save_plots = true
-do_belief = true
+save_plots = false
+do_belief = false
 
 plt[:close]("all")
 pygui(!save_plots)
 
 folder = "chance_constrained"
+
+max_robots = 4
 
 type TrialData
   belief::Histogram
@@ -27,7 +32,8 @@ type TrialData
   feasibility_no_limit
 end
 
-@load "$(folder)/data" data_array csqmi_ratio thetas actuator_limit feasibility_constraint
+@load "$(folder)/data.fixed_probability.40" data_array csqmi_ratio thetas actuator_limit feasibility_constraint
+n_trial = size(data_array, 1)
 
 forces = map(x->x.applied_f, data_array)
 num_measurement = map(x->length(x.robots_measurement), data_array)
@@ -38,9 +44,30 @@ limits = forces - actuator_limits .>-1e-6
 current_feasibility = map(x->x.current_feasibility, data_array)
 true_feasibility = map(x->x.true_feasibility, data_array)
 
-feasible_configuration = num_robot .== 4 .* true_feasibility
-feasible_configurations = sum(feasible_configuration[:,end])
-println("Found $(feasible_configurations) feasible configurations in $(size(data_array,1)) trials")
+
+robots_feasible(robots, theta) =
+  lifting_feasibility(attachment_ws[robots],
+                      theta[3]*g*point_to_wrench(theta[1:2]),
+                      actuator_limit)
+
+current_lifting = Array{Bool}(size(data_array))
+for ii = 1:size(data_array, 1)
+  for jj = 1:size(data_array, 2)
+    current_lifting[ii,jj] = robots_feasible(data_array[ii,jj].robots, thetas[:,ii])
+  end
+end
+
+lifting_configurations = sum(current_lifting[:,end])
+
+println("Found $(lifting_configurations) feasible lifting configurations in $(size(data_array,1)) trials")
+println("$(sum(~true_feasibility[:,end])) trials had no feasible lifting configuration remaining")
+
+##########################################################################
+# compare to behavior of best immediate combination to iterative selection
+##########################################################################
+
+best_initial = map(x->robots_feasible([1,3,5,8], thetas[:,x]), 1:size(thetas,2))
+println("The rate for the best initial configuration is $(mean(best_initial))")
 
 #######################
 # plot normalized error
@@ -50,13 +77,19 @@ println("normalized error")
 figure()
 errors = map(x->x.error, data_array)
 mean_errors = mean(errors, 1)
-@show size(mean_errors)
 
 colors = ["b", "g", "r", "m"]
 
 indices = collect(1:length(mean_errors))
 plot(indices, mean_errors[:], color = "k", linewidth = 2.0)
 
+cutoff = 0.15
+n = 2
+stds = std(errors, 1)[:] / sqrt(size(errors,1))
+fill([indices;reverse(indices)], [mean_errors[:]+stds;reverse(mean_errors[:]-stds)],
+  color = "k", alpha=0.2, linewidth=0.0)
+
+if false
 for ii = 1:size(errors, 1)
   plot(indices, errors[ii,:][:], color = "k", alpha = 0.2,
   linewidth = 1.0)
@@ -65,11 +98,10 @@ for ii = 1:size(errors, 1)
   #scatter(robot_added_inds, errors[jj, ii, robot_added_inds], color =
   #colors[jj], alpha = 0.2, marker = "1")
 
-  #for kk = 2:size(errors, 3)
-    #if
-      #scatter(kk, errors[jj, ii, kk], color = colors[jj], alpha = 0.2)
-    #end
-  #end
+  for kk = 2:size(errors, 3)
+    scatter(kk, errors[jj, ii, kk], color = colors[jj], alpha = 0.2)
+  end
+end
 end
 
 ylim(0.0, 0.5)
@@ -115,11 +147,16 @@ println("feasibility")
 figure()
 
 mean_feasibility = mean(current_feasibility, 1)
-plot(indices, mean_feasibility[:], color = "k", linewidth = 2.0)
+#plot(indices, mean_feasibility[:], color = "k", linewidth = 2.0)
+plot(indices, mean(true_feasibility,1)[:], color = "b")
+plot(indices, mean(current_lifting,1)[:], color = "g")
 
-vs = var(current_feasibility, 1)[:]
-fill([indices;reverse(indices)], [mean_feasibility[:]+vs;reverse(mean_feasibility[:]-vs)],
-  color = "k", alpha=0.2, linewidth=0.0)
+plot([1;size(errors, 2)], max_immediate_feasibility*ones(2), "--r")
+plot([1;size(errors, 2)], mean_immediate_feasibility*ones(2), "--c")
+
+#stds = std(current_feasibility, 1)[:] / sqrt(size(current_feasibility,1))
+#fill([indices;reverse(indices)], [mean_feasibility[:]+stds;reverse(mean_feasibility[:]-stds)],
+  #color = "k", alpha=0.2, linewidth=0.0)
 
 if false
 for ii = 1:size(current_feasibility, 1)
@@ -128,11 +165,53 @@ for ii = 1:size(current_feasibility, 1)
 end
 end
 
+legend(["feasibility existence", "current feasibility", "max configuration", "mean feasibility"], loc="upper left")
+
+
 xlabel("Iteration")
-ylabel("Feasibility probability")
+#ylabel("Feasibility probability")
 
 if save_plots
   matplotlib2tikz.save("$(folder)/feasibility.tex", figureheight="\\figureheight",
+    figurewidth="\\figurewidth")
+end
+
+#################################
+# feasibility by number of robots
+#################################
+
+feasibility_by_number = zeros(max_robots)
+lifting_by_number = zeros(max_robots)
+num_number = zeros(max_robots)
+for ii = 1:max_robots
+  for jj = 1:n_trial
+    ind = findfirst(num_robot[1,:], ii)
+    if ind > 0
+      num_number[ii] += 1
+      feasibility_by_number[ii] += true_feasibility[jj,ind]
+      lifting_by_number[ii] += current_lifting[jj,ind]
+    else
+      feasibility_by_number[ii] += true_feasibility[jj,ind]
+      lifting_by_number[ii] += current_lifting[jj,ind]
+    end
+  end
+end
+
+feasibility_by_number[:] ./= num_number
+lifting_by_number[:] ./= num_number
+
+figure()
+plot(collect(1:max_robots), feasibility_by_number, color = "b")
+plot(collect(1:max_robots), lifting_by_number, color = "g")
+plot([1;max_robots], max_immediate_feasibility*ones(2), "--r")
+plot([1;max_robots], mean_immediate_feasibility*ones(2), "--c")
+legend(["feasibility existence", "current feasibility", "max configuration", "mean feasibility"], loc="upper left")
+
+xlabel("Num. robots")
+#ylabel("Feasibility probability")
+
+if save_plots
+  matplotlib2tikz.save("$(folder)/lifting.tex", figureheight="\\figureheight",
     figurewidth="\\figurewidth")
 end
 
